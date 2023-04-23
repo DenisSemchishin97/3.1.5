4c7a2a50cec76133072d520ef17ca04c453a3066
DenisSemchishin97 <120304966+DenisSemchishin97@users.noreply.github.com>
2023-04-23 19:40:04 +0300

    Update UserService.java

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.entities.Role;
@@ -62,7 +63,9 @@ public class UserService implements UserDetailsService {
         user1.setEmail(user1.getEmail());
         user1.setRoles(user1.getRoles());
         user1.setUsername(user1.getUsername());
-        user1.setPassword(user1.getPassword());
+        String encodedPassword = new BCryptPasswordEncoder().encode(user1.getPassword());
+        user1.setPassword(encodedPassword);
+
         userRepository.save(user1);
     }
 
@@ -85,7 +88,8 @@ public class UserService implements UserDetailsService {
 
     @Transactional
     public void saveUser(User user) {
-
+        String encodedPassword = new BCryptPasswordEncoder().encode(user.getPassword());
+        user.setPassword(encodedPassword);
        userRepository.save(user);
     }
 
